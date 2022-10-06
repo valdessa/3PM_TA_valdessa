f04f64c6df96924941ec238df2fc44187dbdad6e
valdessa <valdessa@esat-libertad.com>
2022-10-06 20:21:00 +0200

    First Exercise

---
+++ Ej1/app/src/main/java/com/example/ej1/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.ej1
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.ej1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_esatlinear);
+    }
+};
