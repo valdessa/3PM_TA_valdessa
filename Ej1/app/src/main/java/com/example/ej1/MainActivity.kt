30824696bf5384ce970d54bf341a1203fcfbc111
valdessa <valdessa@esat-libertad.com>
2022-10-13 20:24:25 +0200

    project updated

--- Ej1/app/src/main/java/com/example/ej1/MainActivity.kt
+++ Ej1/app/src/main/java/com/example/ej1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.ej1
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -13,9 +15,46 @@ import com.example.ej1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_esatlinear);
+        setContentView(R.layout.activity_esatlinear)
+
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
+
+        Log.e("MachineState", "onCreate executed")
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.e("MachineState", "onStart executed")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.e("MachineState", "onResume executed")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.e("MachineState", "onPause executed")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.e("MachineState", "onStop executed")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.e("MachineState", "onDestroy executed")
+    }
+
+
+
 };
